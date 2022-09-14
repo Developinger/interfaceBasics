b3e6e826f7789061c2f7047e449f9d30b0c27750
Homaine Blair <homaineblair@gmail.com>
2022-09-14 13:52:07 -0400

    Initial Commit

---
+++ interfaceBasics/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  interfaceBasics
+//
+//  Created by 1a on 9/14/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
