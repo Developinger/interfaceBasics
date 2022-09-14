212367a19c7c663202aa64cb06e56d77ca619c5d
Homaine Blair <homaineblair@gmail.com>
2022-09-14 14:19:31 -0400

    v1

---
+++ InterfaceBasicsOS/InterfaceBasicsOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InterfaceBasicsOS
+//
+//  Created by 1a on 9/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userNameINP: UITextField!
+    
+    @IBOutlet weak var greetCompiler: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func greetButtonTouched(_ sender: Any) {
+    }
+    
+}
+
